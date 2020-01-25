539bb34f9c48764e6bbe6b3ae90a135ea17556d2
bijaybdrkarki <bijay.jhupro@gmail.com>
2020-01-24 19:08:41 -0500

    server setup node-express and handlebar

---
+++ server.js
@@ -0,0 +1,23 @@
+//imports the express package that was installed within your application
+const express = require("express"); 
+
+const app = express(); // create express object ---> app
+const PORT = 3000; // port number for localhost
+
+
+// imports the handlebar package
+const exphbs  = require('express-handlebars');
+
+//register handlebars view engine
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+// Creates an Express Web Server that listens to HTTP Reuqest on port 3000
+app.listen(PORT, () => {
+    console.log(`Web Server Started`); 
+})
+
+//routes
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
