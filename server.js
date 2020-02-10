70f7c4d56e7e2b4a6546b05784020591633a8c7e
bijaybdrkarki <bijay.jhupro@gmail.com>
2020-02-10 00:25:58 -0500

    route post for signup

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 //imports the express package that was installed within your application
 const express = require("express"); 
 
+// imports the handlebar package
+const exphbs  = require('express-handlebars');
+
+const bodyParser = require('body-parser');
 const app = express(); // create express object ---> app
 const PORT = 3000; // port number for localhost
 
+const fakedb=[];
 //This allows express to make my static content avialable from the public
 app.use(express.static('public'))
 
-// imports the handlebar package
-const exphbs  = require('express-handlebars');
+app.use(bodyParser.urlencoded({ extended: false }))
+
+
 
 //register handlebars view engine
 app.engine('handlebars', exphbs());
@@ -31,6 +37,62 @@ app.get('/signup-or-login', (req, res) => {
 })
 app.get('/signup', (req, res) => {
     res.render("signup")
+})
+app.post("/signup", (req,res)=>{
+    // console.log("test");
+    let errormessg=[];
+    const errors={};
+    if(req.body.email=="")
+    {
+    errors.email = "Sorry, you must enter email address";
+    }
+    if(req.body.firstName=="")
+    {
+    errors.firstName= "Sorry, you must enter a first name";
+    }
+    if(req.body.lastName=="")
+    {
+    errors.lastName="Sorry, you must enter last name";
+    }
+    if(req.body.password=="")
+    {
+    errors.password="Sorry, you must enter a password";
+    }
+    if(req.body.confirmPassword=="")
+    {
+    errors.confirmPassword="Sorry, you must enter matching password";
+    }
+    if(req.body.month== undefined)
+    {
+    errors.month="Sorry, you must select month";
+    }
+    if(req.body.day== undefined)
+    {
+    errors.day="Sorry, you must select day";
+    }
+    if(req.body.year== undefined)
+    {
+    errors.year="Sorry, you must select year";
+    }
+    if( req.body.password.slice(1).match(/[0-9a-zA-Z]+$/) )
+    {
+        errors.passType = "password must have at least 8 characters with first letter capital & all other alphanumeric"
+    }
+    if(req.body.password.length > 0 && req.body.confirmPassword.length > 0 && req.body.password !== req.body.confirmPassword)
+    {
+        errors.match= "password not matched";
+    }
+    errormessg.push(errors);
+    // console.log(errors);
+    if(errormessg.length > 0)
+    {
+    //    console.log("inside error.length");
+        res.render("signup",{
+        messages : errors
+        })
+    }
+    
+    // req.body.password.length > 0 && req.body.password.length < 8 || req.body.password[0] != req.body.password[0].toUpperCase() ||
 })
 app.get('/login', (req, res) => {
     res.render("login")
