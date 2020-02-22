4dc95afc239c780b7fac3627b2a3d21219d83687
bijaybdrkarki <bijay.jhupro@gmail.com>
2020-02-21 21:42:13 -0500

    use of modular routes

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const bodyParser = require('body-parser');
 const app = express(); // create express object ---> app
 const PORT = 3000; // port number for localhost
 
+//import routes from routes folder
+const index = require('./routes/index');
+const signup = require('./routes/signup');
 const fakedb=[];
 //This allows express to make my static content avialable from the public
 app.use(express.static('public'))
@@ -24,83 +27,13 @@ app.listen(PORT, () => {
 })
 
 //routes
-app.get('/', (req, res) => {
-    res.render("index",{
-        title: "Home",
-        headingInfo : "Home Page",
-    })
-})
+app.use('/', index);
+app.use('/signup', signup);
+
 app.get('/signup-or-login', (req, res) => {
     res.render("signupLogin")
 })
-app.get('/signup', (req, res) => {
-    res.render("signup")
-})
-app.post("/signup", (req,res)=>{
-    // console.log("test");
-    let errormessg=[];
-    let formdata={
-        email : req.body.email,
-        fname : req.body.firstName,
-        lname : req.body.lastName,
-        day : req.body.day,
-        month : req.body.month,
-        year : req.body.year
-    }; 
-    const errors={};
-    if(req.body.email=="")
-    {
-    errors.email = "Sorry, you must enter email address";
-    }
-    if(req.body.firstName=="")
-    {
-    errors.firstName= "Sorry, you must enter a first name";
-    }
-    if(req.body.lastName=="")
-    {
-    errors.lastName="Sorry, you must enter last name";
-    }
-    if(req.body.password=="")
-    {
-    errors.password="Sorry, you must enter a password";
-    }
-    if(req.body.confirmPassword=="")
-    {
-    errors.confirmPassword = "Sorry, you must enter matching password";
-    }
-    if(req.body.month == undefined)
-    {
-    errors.month="Sorry, you must select month";
-    }
-    if(req.body.day == undefined)
-    {
-    errors.day="Sorry, you must select day";
-    }
-    if(req.body.year == undefined)
-    {
-    errors.year="Sorry, you must select year";
-    }
-    if( req.body.password.length < 8 || req.body.password[0] != req.body.password[0].toUpperCase() || ! (/^[0-9a-zA-Z]+$/).test(req.body.password.slice(1)) )
-    {
-        errors.passType = "password must have at least 8 characters with first letter capital & all other alphanumeric"
-    }
-    if(req.body.password.length > 0 && req.body.confirmPassword.length > 0 && req.body.password !== req.body.confirmPassword)
-    {
-        errors.match= "password not matched";
-    }
-    errormessg.push(errors);
-    
-    if(errormessg.length > 0)
-    {
-    //    console.log("inside error.length");
-        res.render("signup",{
-        messages : errors,
-        data : formdata
-        })
-    }
-    
-    
-})
+
 app.get('/login', (req, res) => {
     res.render("login")
 })
