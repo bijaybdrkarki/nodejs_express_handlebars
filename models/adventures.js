061cac6507a13847cdaeb7921fe3a1c15ad52769
bijaybdrkarki <bijay.jhupro@gmail.com>
2020-02-23 21:34:44 -0500

    adventure section models data

---
+++ models/adventures.js
@@ -0,0 +1,34 @@
+const adventures = [
+    {
+        title : 'ADV 1',
+        price : 120.00,
+        image : '/img/adv1.jpeg'
+    },
+    {
+        title : 'ADV 2',
+        price : 100.00,
+        image : '/img/adv2.jpg'
+    },
+    {
+        title : 'ADV 3',
+        price : 90.00,
+        image : '/img/adv3.jpg'
+    },
+    {
+        title : 'ADV 4',
+        price : 150.00,
+        image : '/img/adv4.jpg'
+    },
+    {
+        title : 'ADV 5',
+        price : 200.00,
+        image : '/img/adv5.jpg'
+    },
+    {
+        title : 'ADV 6',
+        price : 80.00,
+        image : '/img/adv6.jpg'
+    }
+]
+
+module.exports = adventures;
