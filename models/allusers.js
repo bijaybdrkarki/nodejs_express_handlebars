9f210ad8192398e52c41b0a048aaa3f23a5bed32
bijaybdrkarki <bijay.jhupro@gmail.com>
2020-04-10 17:30:47 -0400

    alluser schema and model

--- models/allusers.js
+++ models/allusers.js
@@ -1,16 +1,92 @@
-const fs = require("fs");
-const allusers = [];
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const bcrypt = require('bcryptjs');
 
-let data = fs.readFileSync("models/database.txt", "utf8");
-let jsondata = JSON.parse(data);
+const alluserSchema = new Schema({
 
-if (data.length > 1)
-{
-    for(let i=0 ; i<jsondata.length; i++)
+    email:
     {
-        allusers.push(jsondata[i]);
+        type: String,
+        required: true
+    },
+    fname:
+    {
+        type: String,
+        required: true
+    },
+    lname:
+    {
+        type: String,
+        required: true
+    },
+    password:
+    {
+        type: String,
+        required: true
+    },
+    phone:
+    {
+        type: String,
+        required: true
+    },
+    day:
+    {
+        type: String,
+        required: true
+    },
+    month:
+    {
+        type: String,
+        required: true
+    },
+    year:
+    {
+        type: String,
+        required: true
+    },
+    userImg:
+    {
+        type: String
+    },
+    dateCreated:
+    {
+        type: Date,
+        default: Date.now()
     }
-    
-}
 
-module.exports = allusers;
+});
+
+alluserSchema.pre("save",function(next)
+{
+    bcrypt.genSalt(10)
+    .then((salt)=>{
+        bcrypt.hash(this.password,salt)
+        .then((encryptPassword)=>{
+            this.password = encryptPassword;
+            next();
+        })
+        .catch(err => console.log(`error occured during hashing of password encryption ${err}`))
+    })
+    .catch(err => console.log(`error occured during encrypting ${err}`))
+})
+const alluserModel = mongoose.model('alluser', alluserSchema)
+
+module.exports = alluserModel;
+
+
+
+// const fs = require("fs");
+// const allusers = [];
+
+// let data = fs.readFileSync("models/database.txt", "utf8");
+// let jsondata = JSON.parse(data);
+
+// if (data.length > 1)
+// {
+//     for(let i=0 ; i<jsondata.length; i++)
+//     {
+//         allusers.push(jsondata[i]);
+//     }
+    
+// }
+
