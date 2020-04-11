59016b445dc06e023158042d76c35702402d49ed
bijaybdrkarki <bijay.jhupro@gmail.com>
2020-04-11 15:21:31 -0400

    import data from database and inject to handlebar

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,12 +1,30 @@
 const express = require("express"); 
-const allrooms =require('../models/allrooms');
+const allRoomModel = require('../models/allrooms');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render("rooms",{
-        title: "All rooms",
-        rooms : allrooms
+    allRoomModel.find()
+    .then((rooms)=>{
+        const filteredRoom = rooms.map((room)=>{
+            return {
+                id: room._id,
+                name: room.name,
+                description: room.description,
+                extra: room.extra,
+                price: room.price,
+                rating: room.rating,
+                roomImg: room.roomImg,
+            }
+        })
+        
+        res.render("rooms",{
+            title: "All rooms",
+            rooms : filteredRoom
+        })
     })
+    .catch( err => console.log(`error occured while retrieving room to database ${err}`));
+    
+    
 })
 
 
