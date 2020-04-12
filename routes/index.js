b5a83ef6f4e70d2d3a7527036feccb65a54f7fb7
bijaybdrkarki <bijay.jhupro@gmail.com>
2020-04-11 23:57:24 -0400

    added featured room section from database

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,36 @@
 const express = require("express"); 
 const router = express.Router();
+const allRoomModel = require('../models/allrooms');
 
 router.get('/', (req, res) => {
-    res.render("index",{
-        title: "Home",
-        headingInfo : "Home Page",
+    
+    allRoomModel.find({featuredRoom : true})
+    .then((rooms)=>{
+        const filteredRoom = rooms.map((room)=>{
+            return {
+                id: room._id,
+                name: room.name,
+                description: room.description,
+                extra: room.extra,
+                price: room.price,
+                rating: room.rating,
+                featuredRoom: room.featuredRoom,
+                roomImg: room.roomImg,
+            }
+        })
+        
+        res.render("index",{
+            title: "Home",
+            headingInfo : "Home Page",
+            featured: filteredRoom
+        })
     })
+    .catch( err => console.log(`error occured while retrieving room to database ${err}`));
+    
+    
+    
+    
+    
 })
 
 
