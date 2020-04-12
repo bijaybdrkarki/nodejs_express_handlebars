c610a9192d923e99c7566bf274042050433e0008
bijaybdrkarki <bijay.jhupro@gmail.com>
2020-04-12 15:22:05 -0400

    render admin-allrooms page that was rename earlier

--- routes/admin.js
+++ routes/admin.js
@@ -29,7 +29,7 @@ router.get('/room', isAuthenticate, isAdmin, (req, res) => {
             }
         })
         
-        res.render("room",{
+        res.render("adminAllroom",{
             title: "Admin-allroom",
             data: filteredRoom
         })
